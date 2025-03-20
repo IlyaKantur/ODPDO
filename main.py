2965f9a67a828cd74bc8e6549b95079b56c8a0f0
IlyaKantur <il.kantur@mail.ru>
2025-03-20 13:52:43 +0500

    0.2

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ class window(QMainWindow):
 
         # Переменные
         self.Folder_path_1D = ""
-        self.Name_File_1D = ""
+        self.Name_File_1D = []
         self.File_path_1D = []
+        self.data_files = []  # Массив для хранения данных из файлов
         self.cor_X_File_1D = []
         self.mas_Y_File_1D = []
         self.mas_sum_1D = []
@@ -38,6 +39,7 @@ class window(QMainWindow):
         # Тригеры к кнопкам
         self.ui.ClearConsole_pushButton.clicked.connect(self.console_clear) #Очистка консоли
         self.ui.Folder_pushButton.clicked.connect(self.Folder_pushButton)
+        self.ui.Files_pushButton.clicked.connect(self.Files_pushButton)
 
         # Создаем PlotWidget для результатов
         self.plot_widget_resoult = pg.PlotWidget()
@@ -51,9 +53,8 @@ class window(QMainWindow):
         self.plot_widget_graphs = pg.PlotWidget()
         self.ui.graphs_gridLayout.addWidget(self.plot_widget_graphs)
 
-        # # Создаем линию графика таблицы
-        # self.curve = self.plot_widget_table.plot(self.table_X, self.table_Y)
-
+        # Подключаем обработчик события для нажатия на ячейку таблицы
+        self.ui.table_tableWidget.cellClicked.connect(self.on_table_cell_clicked)
 
         self.console("Программа запущена", False)
 
@@ -63,60 +64,118 @@ class window(QMainWindow):
 
         # Проверяем, была ли выбрана папка
         if folder_path:
-            # Записываем путь к выбранной папке
-            self.Folder_path_1D = folder_path
-            
+            # Нормализуем путь к папке
+            self.Folder_path_1D = os.path.normpath(folder_path)
+
             # Получаем список всех файлов в выбранной папке
-            fileList = os.listdir(folder_path)
-            if fileList:
-                # Фильтруем файлы по расширениям .dat и .txt
-                valid_extensions = ('.dat', '.txt')
-                self.File_path_1D = [os.path.join(folder_path, file) for file in fileList if file.endswith(valid_extensions)]
-                self.Name_File_1D = [file for file in fileList if file.endswith(valid_extensions)]
+            self.loadFilesFromDirectory(self.Folder_path_1D)
 
-                # Проверяем, есть ли подходящие файлы
-                if not self.Name_File_1D:
-                    self.console("Файлов подходящего формата (dat, txt) нет", True)  # Выводим сообщение об ошибке
-                else:
-                    self.ui.table_tableWidget.clear()  # Очищаем таблицу перед добавлением новых данных
-                    self.ui.table_tableWidget.setRowCount(len(self.Name_File_1D))  # Устанавливаем количество строк
+    def Files_pushButton(self):
+        # Открываем диалоговое окно для выбора файлов
+        file_paths, _ = QtWidgets.QFileDialog.getOpenFileNames(self, "Выберите файлы", "", "Data Files (*.dat *.txt)")
 
-                    # Заполняем таблицу именами файлов
-                    for row, file_name in enumerate(self.Name_File_1D):
-                        self.ui.table_tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(file_name))  # Добавляем имя файла в первую колонку
+        # Проверяем, были ли выбраны файлы
+        if file_paths:
+            # Нормализуем пути к файлам
+            self.File_path_1D = [os.path.normpath(file) for file in file_paths]
+            self.Name_File_1D = [os.path.basename(file) for file in self.File_path_1D]
 
-                    text = f'Загружено: {len(self.Name_File_1D)} файлов'
-                    self.console(text, False)
+            # Загружаем данные из выбранных файлов
+            self.loadFilesData()
 
-                    # Читаем данные из файлов и строим графики
-                    self.loadAndPlotData()
+    def loadFilesFromDirectory(self, folder_path):
+        # Получаем список всех файлов в выбранной папке
+        fileList = os.listdir(folder_path)
+        if fileList:
+            # Фильтруем файлы по расширениям .dat и .txt
+            valid_extensions = ('.dat', '.txt')
+            self.File_path_1D = [os.path.normpath(os.path.join(folder_path, file)) 
+                                for file in fileList if file.endswith(valid_extensions)]
+            self.Name_File_1D = [file for file in fileList if file.endswith(valid_extensions)]
+
+            # Проверяем, есть ли подходящие файлы
+            if not self.Name_File_1D:
+                self.console("Файлов подходящего формата (dat, txt) нет", True)  # Выводим сообщение об ошибке
             else:
-                self.console("Папка пустая", False)
-                    
+                self.data_files = []  # Очищаем массив данных
+                self.ui.table_tableWidget.clear()  # Очищаем таблицу перед добавлением новых данных
+                self.ui.table_tableWidget.setRowCount(len(self.Name_File_1D))  # Устанавливаем количество строк
+
+                # Заполняем таблицу именами файлов и загружаем данные
+                for row, file_name in enumerate(self.Name_File_1D):
+                    self.ui.table_tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(file_name))  # Добавляем имя файла в первую колонку
+                    file_path = self.File_path_1D[row]
+                    data = self.readDataFromFile(file_path)
+                    if data is not None:
+                        self.data_files.append(data)  # Сохраняем данные в массив
+
+                text = f'Загружено: {len(self.Name_File_1D)} файлов'
+                self.console(text, False)
+
+                # Строим графики для всех загруженных данных
+                self.loadAndPlotData()
+        else:
+            self.console("Папка пустая", False)
+
+    def loadFilesData(self):
+        # Очищаем массив данных
+        self.data_files = []
+        self.ui.table_tableWidget.clear()  # Очищаем таблицу перед добавлением новых данных
+        self.ui.table_tableWidget.setRowCount(len(self.Name_File_1D))  # Устанавливаем количество строк
+
+        # Заполняем таблицу именами файлов и загружаем данные
+        for row, file_name in enumerate(self.Name_File_1D):
+            self.ui.table_tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(file_name))  # Добавляем имя файла в первую колонку
+            file_path = self.File_path_1D[row]
+            data = self.readDataFromFile(file_path)
+            if data is not None:
+                self.data_files.append(data)  # Сохраняем данные в массив
+
+        text = f'Загружено: {len(self.Name_File_1D)} файлов'
+        self.console(text, False)
+
+        # Строим графики для всех загруженных данных
+        self.loadAndPlotData()
 
     def loadAndPlotData(self):
         # Очищаем предыдущие графики
         self.plot_widget_graphs.clear()
 
-        for file_path in self.File_path_1D:
-            # Чтение данных из файла
-            data = self.readDataFromFile(file_path)
-            if data is not None:
-                self.plotData(data, file_path)
+        for data, file_path in zip(self.data_files, self.File_path_1D):
+            # Строим график для каждого файла
+            self.plotData(data, file_path)
 
     def plotData(self, data, file_path):
         # Предполагаем, что данные имеют два столбца: X и Y
         x, y = data
-        # Строим график
-        self.plot_widget_graphs.plot(x, y, pen='r', name=os.path.basename(file_path))
+        # Устанавливаем начальное смещение по оси Y
+        y_offset = 30
+        # Получаем текущее количество графиков
+        current_plot_count = len(self.plot_widget_graphs.listDataItems())
+        # Применяем смещение к Y
+        y_shifted = y + current_plot_count * y_offset
+        # Строим график с учетом смещения
+        self.plot_widget_graphs.plot(x, y_shifted, pen='r', name=os.path.basename(file_path))
 
     def readDataFromFile(self, file_path):
         try:
-            # Чтение данных из файла (предполагается, что данные разделены пробелом)
-            data = np.loadtxt(file_path, delimiter=' ')  # Измените разделитель, если необходимо
-            return data[:, 0], data[:, 1]  # Возвращаем два столбца: X и Y
+            # Для хранения информации из заголовка
+            header_info = []
+            header_lines = 0         
+            with open(file_path, 'r') as file:
+                for line in file:
+                    try:
+                        values = [float(val) for val in line.strip().split()]
+                        if len(values) >= 2:
+                            break
+                    except ValueError:
+                        header_info.append(line.strip())
+                        header_lines += 1
+            # Читаем данные, пропуская строки заголовка
+            data = np.loadtxt(file_path, delimiter=' ', skiprows=header_lines)           
+            return data[:, 0], data[:, 1]
         except Exception as e:
-            self.console(f"Ошибка при чтении файла {file_path}: {str(e)}", True)
+            self.console(f"Ошибка при чтении файла {os.path.basename(file_path)}: {str(e)}", True)
             return None
 
     #  Выводит иформацию в консоль
@@ -138,9 +197,21 @@ class window(QMainWindow):
     def console_clear(self):
         self.ui.Console_textEdit.clear() 
 
-            
+    def on_table_cell_clicked(self, row, column):
+        # Получаем данные из массива по выбранной ячейке
+        if row < len(self.data_files):
+            data = self.data_files[row]
+            # Строим график в plot_widget_table
+            self.plot_widget_table.clear()  # Очищаем предыдущий график
+            x, y = data
+            self.plot_widget_table.plot(x, y, pen='b', name=self.Name_File_1D[row])  # Строим график
 
-
+            # Выводим координаты в CoordinatTable_tableWidget
+            self.ui.CoordinatTable_tableWidget.clear()  # Очищаем предыдущие координаты
+            self.ui.CoordinatTable_tableWidget.setRowCount(len(y))  # Устанавливаем количество строк
+            for i in range(len(y)):
+                self.ui.CoordinatTable_tableWidget.setItem(i, 0, QtWidgets.QTableWidgetItem(str(x[i])))  # X координаты
+                self.ui.CoordinatTable_tableWidget.setItem(i, 1, QtWidgets.QTableWidgetItem(str(y[i])))  # Y координаты
 
 app = QtWidgets.QApplication([])
 mainWin = window()
